715185c1c3afe1a583485eae41643cd709205923
mahindra <kodalimahindra3072@gmail.com>
2021-07-16 06:31:18 +0000

    changed mail server

--- django_webapp/settings.py
+++ django_webapp/settings.py
@@ -143,9 +143,9 @@ LOGIN_URL ='login'
 
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 
-EMAIL_HOST ='smtp.gmail.com'
+EMAIL_HOST ='smtp.bizmail.yahoo.com'
 
-EMAIL_PORT = 587
+EMAIL_PORT =587
 
 EMAIL_USE_TLS = True
 
@@ -153,4 +153,6 @@ EMAIL_HOST_USER = config.get('EMAIL_USER')
 
 
 EMAIL_HOST_PASSWORD = config.get('EMAIL_PASS')
+
+DEFAULT_FROM_EMAIL='no-reply@tech4umate.net'
 
